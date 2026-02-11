c54306ebdc576f0595fdbb370cf756a9de422074
Tigerfrog-01 <kristopherzuravljov005@gmail.com>
2026-02-11 09:19:29 +0200

    Lisasin sisu StartPage-TextPage jaoks

---
+++ Projekt2_TARpe24_Kristopher/StartPage.xaml.cs
@@ -0,0 +1,37 @@
+namespace Projekt2_TARpe24_Kristopher;
+
+public partial class StartPage : ContentPage
+{
+    VerticalStackLayout vst;
+    ScrollView sv;
+    public List<ContentPage> Lehed = new List<ContentPage>() { new TextPage(), new FigurePage() };
+    public List<string> LeheNimed = new List<string>() { "Tekst", "Kujund" };
+
+    public StartPage()
+    {
+        
+        vst = new VerticalStackLayout { Padding = 20, Spacing = 15 };
+        for (int i = 0; i < Lehed.Count; i++)
+        {
+            Button nupp = new Button
+            {
+                Text = LeheNimed[i],
+                FontSize = 36,
+                FontFamily = "Luffio",
+                BackgroundColor = Colors.LightGray,
+                TextColor = Colors.Black,
+                CornerRadius = 10,
+                HeightRequest = 60,
+                ZIndex = i
+            };
+            vst.Add(nupp);
+            nupp.Clicked += (sender, e) =>
+            {
+                var valik = Lehed[nupp.ZIndex];
+                Navigation.PushAsync(valik);
+            };
+        }
+        sv = new ScrollView { Content = vst };
+        Content = sv;
+    }
+}
