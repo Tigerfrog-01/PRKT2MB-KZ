c664cae4d8def78eb42c350d092c3974da178522
Opilane TTHK <opilane@tthk.ee>
2026-02-26 09:58:50 +0200

    Proovin Lumemme tööle saada

--- Projekt2_TARpe24_Kristopher/Lumememm.xaml.cs
+++ Projekt2_TARpe24_Kristopher/Lumememm.xaml.cs
@@ -1,3 +1,6 @@
+using static Android.Graphics.ImageDecoder;
+using static Java.Util.Jar.Attributes;
+
 namespace Projekt2_TARpe24_Kristopher;
 
 public partial class Lumememm : ContentPage
@@ -5,7 +8,32 @@ public partial class Lumememm : ContentPage
 	public Lumememm()
 	{
 		InitializeComponent();
+
+		Melt.Clicked += async (s, e) =>
+		{
+
+			Grid2.FadeTo(0, 2000, Easing.Linear);
+            Grid3.FadeTo(0, 2000, Easing.Linear);
+            Grid4.FadeTo(0, 2000, Easing.Linear);
+
+
+            await Paike.FadeTo(1, 2000, Easing.CubicIn);
+
+
+        };
+
+
+}
+
+
+
+
 	}
+	
+	
+	
+	
+	
+
 
 	
-}
